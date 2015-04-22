cd11ea70111802faa80171c99d3ce751d3d6fbf1
Erwin Elling <erwinelling@gmail.com>
2015-04-22 21:11:39 +0200

    First version
    No menu, changed text on homepage, etc.

---
+++ assets/js/indextws.js
@@ -0,0 +1,16 @@
+/**
+ * Extra JS file for Casper behaviours for TWS
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+        $(".scroll").arctic_scroll();
+        
+    });
+})(jQuery);
